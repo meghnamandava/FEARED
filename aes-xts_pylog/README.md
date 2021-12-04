d7fcb867cc66fdef14829349b2d066c8b61ca163
Scott <ssscrazy@live.com>
2021-12-04 15:58:31 -0600

    initial version of pylog that runs but aint correct sadge

---
+++ aes-xts_pylog/README.md
@@ -0,0 +1,1 @@
+from https://github.com/jonghwanhyeon/xts-aes-256
---
+++ aes-xts_pylog/main.py
@@ -0,0 +1,239 @@
+import sys
+import binascii
+from pylog import *
+
+from xts_aes import XTSAES
+
+import aes_tables
+
+#@pylog(board='pynq')
+def xts_aes(key, tweak, text, mode) :
+
+    def aes_process(text, initial_round, round_factor, expanded_key):
+        state_matrix = aes_sequence_to_matrix(text)
+        round = initial_round
+
+        aes_add_round_key(state_matrix, aes_get_round_key(round, expanded_key))
+        round += round_factor
+
+        for i in range(0, 13):
+            aes_substitute_bytes(state_matrix, aes_tables.s_boxes[round_factor])
+            aes_shift_rows(state_matrix, round_factor)
+            aes_mix_columns(state_matrix, aes_tables.mix_column_constant_matrices[round_factor])
+
+            round_key = aes_get_round_key(round, expanded_key)
+            if round_factor == -1: aes_mix_columns(round_key, aes_tables.mix_column_constant_matrices[round_factor])
+            aes_add_round_key(state_matrix, round_key)
+
+            round += round_factor
+
+        aes_substitute_bytes(state_matrix, aes_tables.s_boxes[round_factor])
+        aes_shift_rows(state_matrix, round_factor)
+        aes_add_round_key(state_matrix, aes_get_round_key(round, expanded_key))
+
+        return aes_matrix_to_sequence(state_matrix)
+
+    def aes_substitute_bytes(state_matrix, s_box):
+        for row_index in range(0, 4):
+            for column_index in range(0, 4):
+                state_matrix[row_index][column_index] = s_box[state_matrix[row_index][column_index]]
+
+        return state_matrix
+
+    def aes_shift_rows(state_matrix, round_factor):
+        for row_index in range(1, 4):
+            state_matrix[row_index] = state_matrix[row_index][round_factor * row_index:] + state_matrix[row_index][:round_factor * row_index]
+
+        return state_matrix
+
+    def aes_mix_columns(state_matrix, constant_matrix):
+        for column_index in range(0, 4):
+            column = bytearray([
+                state_matrix[0][column_index], 
+                state_matrix[1][column_index], 
+                state_matrix[2][column_index], 
+                state_matrix[3][column_index]
+            ])
+
+            for row_index in range(0, 4):
+                state_matrix[row_index][column_index] = aes_tables.multiplication[constant_matrix[row_index][0]][column[0]]
+                state_matrix[row_index][column_index] ^= aes_tables.multiplication[constant_matrix[row_index][1]][column[1]]
+                state_matrix[row_index][column_index] ^= aes_tables.multiplication[constant_matrix[row_index][2]][column[2]]
+                state_matrix[row_index][column_index] ^= aes_tables.multiplication[constant_matrix[row_index][3]][column[3]]
+
+        return state_matrix
+
+    def aes_add_round_key(state_matrix, round_key_matrix):
+        for row_index in range(0, 4):
+            for column_index in range(0, 4):
+                state_matrix[row_index][column_index] ^= round_key_matrix[row_index][column_index]
+
+        return state_matrix
+
+    def aes_get_round_key(round, expanded_key):
+        key_column_index = 4 * round
+        round_key = [
+            expanded_key[0][key_column_index:key_column_index + 4],
+            expanded_key[1][key_column_index:key_column_index + 4],
+            expanded_key[2][key_column_index:key_column_index + 4],
+            expanded_key[3][key_column_index:key_column_index + 4],
+        ]
+
+        return round_key
+
+    def aes_expand_key(key):
+        expanded_key = bytearray(key)
+        n = 1
+
+        step = 0
+        while len(expanded_key) < 240:
+            temporary_key = expanded_key[-4:]
+
+            if step == 0:
+                # rotate word
+                temporary_key = temporary_key[1:] + temporary_key[:1]
+
+                # substitue word
+                for i in range(0, 4):
+                    temporary_key[i] = aes_tables.s_box[temporary_key[i]]
+
+                # xor round constant
+                temporary_key[0] ^= aes_tables.rcon[n]
+
+                n += 1
+            elif step == 4:
+                for i in range(0, 4):
+                    temporary_key[i] = aes_tables.s_box[temporary_key[i]]
+
+            for i in range(0, 4):
+                temporary_key[i] ^= expanded_key[-32 + i]
+
+            expanded_key += temporary_key
+            step = (step + 1) % 8
+
+        return aes_sequence_to_matrix(expanded_key)
+
+    def aes_sequence_to_matrix(sequence):
+        matrix = [bytearray(), bytearray(), bytearray(), bytearray()]
+
+        row = 0
+        for element in sequence:
+            matrix[row].append(element)
+            row = (row + 1) % 4
+
+        return matrix
+
+    def aes_matrix_to_sequence(matrix):
+        sequence = bytearray()
+
+        column_length = len(matrix[0])
+        for column in range(0, column_length):
+            for row in range(0, 4):
+                sequence.append(matrix[row][column])
+
+        return sequence
+    
+    def xts_aes_calculate_next_tweak(tweak):
+        next_tweak = bytearray()
+
+        carry_in = 0
+        carry_out = 0
+
+        for j in range(0, 16):
+            carry_out = (tweak[j] >> 7) & 1
+            next_tweak.append(((tweak[j] << 1) + carry_in) & 0xFF)
+            carry_in = carry_out
+
+        if carry_out:
+            next_tweak[0] ^= 0x87
+
+        return next_tweak
+
+    def xts_aes_process_block(block, tweak, mode, expanded_key):
+        new_block = map(lambda x, y: x ^ y, block, tweak)
+        if mode == 1: # encrypt
+            new_block = aes_process(block, 0, 1, expanded_key)
+        else : # decrypt
+            new_block = aes_process(block, 14, -1, expanded_key)
+
+        new_block = map(lambda x, y: x ^ y, new_block, tweak)
+
+        return bytearray(new_block)
+
+    def xts_aes_process_data(data, mode, tweak, expanded_key):
+
+        blocks = [data[i:i + 16] for i in range(0, len(data), 16)]
+        if len(blocks[-1]) == 16:
+            blocks.append(b'')
+
+        for i in range(0, len(blocks) - 2):
+            blocks[i] = xts_aes_process_block(blocks[i], tweak, mode, expanded_key)
+            tweak = xts_aes_calculate_next_tweak(tweak)
+
+        partial_length = len(blocks[-1])
+        if partial_length == 0: # multiple of block size
+            blocks[-2] = xts_aes_process_block(blocks[-2], tweak, mode, expanded_key)
+        else: # not multiple of block size
+            if mode == 1: # in-order
+                first_tweak = tweak
+                second_tweak = xts_aes_calculate_next_tweak(tweak)
+            else: # reverse-order
+                second_tweak = tweak
+                first_tweak = xts_aes_calculate_next_tweak(tweak)
+
+            cc = xts_aes_process_block(blocks[-2], first_tweak, mode, expanded_key)
+            pp = blocks[-1] + cc[partial_length:]
+            blocks[-1] = cc[:partial_length]
+            blocks[-2] = xts_aes_process_block(pp, second_tweak, mode, expanded_key)
+
+        return b''.join(blocks)
+
+    aes_expanded_key = aes_expand_key(key[0:32])
+    aes_expanded_key2 = aes_expand_key(key[32:64])
+    aes_tweak = aes_process(tweak, 0, 1, aes_expanded_key2)
+
+    return xts_aes_process_data(text, mode, aes_tweak, aes_expanded_key)
+
+# end xts_aes
+
+if __name__ == "__main__":
+    TEXT_TYPES = {
+        'encryption': 'plaintext',
+        'decryption': 'ciphertext',
+    }
+
+    def read_hex_string(name):
+        try:
+            hex_string = input('{name}: '.format(name=name))
+            hex_string = binascii.unhexlify(hex_string)
+        except binascii.Error:
+            sys.exit('{name} should be hex string'.format(name=name))
+
+        return hex_string
+
+    arguments = sys.argv[1:]
+    mode = 'encryption'
+    inverse_mode = 'decryption'
+    if arguments and (arguments[0] == '-d'):
+        mode = 'decryption'
+        inverse_mode = 'encryption'
+
+    key = read_hex_string('key')
+    if len(key) != 64:
+        sys.exit('key should be 64-byte')
+
+    tweak = read_hex_string('tweak')
+    if len(tweak) != 16:
+        sys.exit('tweak should be 16-byte')
+
+    text = read_hex_string(TEXT_TYPES[mode])
+    if len(text) < 16:
+        sys.exit('{text_type} should be greater than or equal to 16-byte'.format(text_type=TEXT_TYPES[mode]))
+
+    res = -1
+    if mode == 'encryption' :
+        res = xts_aes(key, tweak, text, 1)
+    else :
+        res = xts_aes(key, tweak, text, 0)
+
+    print('{ciphertext_type}: {ciphertext}'.format(ciphertext_type=TEXT_TYPES[inverse_mode], ciphertext=binascii.hexlify(res).decode()))
